0603896c463adc107ff889cf9c13fb2012fca0ac
firzanrisnanda <firzanrisnanda24@gmail.com>
2019-01-23 18:30:03 +0700

    kakao

--- DGS.php
+++ DGS.php
@@ -1,173 +1,174 @@
-<?php
-include "config/koneksi.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link href="favicon.ico" rel="shortcut icon">
+	<!-- CSS dasar -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/custom.css">
+    <link rel="icon" type="image/png" href="images/logo.png">
+	<title>pakar kakao</title> 
+</head>
+<body>
+    <div class="container">
+    <header class="header clearfix">
+		<div class="article clearfix">
+			<div class="col_100 clearfix">
+				<div class="logo"><img src="images/logo.png" /></div>
+			</div>
+		</div>
+    </header>
+	<div class="hero clearfix">
+		<div class="col_100">
+			<ul id="nav">
+	           	<li><a href="index.php">Home</a></li>
+				<li><a href="Diagnosa.php">Diagnosa</a></li>
+	            <li><a href="info.php">Info</a>
+	            <li><a href="saran.php">Saran</a>
+				<li><a href="About.php">About</a>
+				<li><a href="Help.php">Help</a>
+	            <li><a href="admin/login.php">Login</a></li>
+	        </ul>
+		</div>
+		<div class="clearfix"></div>
+	</div>
 
-$dlpenyakit = $_POST['Gejala'];
-echo json_encode($dlpenyakit);
-?>
-<table class="table table-bordered">
-            <thead>
-              <tr>
-          
-				<th class="align-left">ID Rule</th>
-                <th class="align-left">ID Gejala</th>
-                <th class="align-left">ID penyakit</th>
-				<th class="align-left">Probabilitas gejala</th>
-              </tr>
-			  </thead>
-</table>			  
-<?php
-foreach($dlpenyakit as $ddata){
-	$sql   = "SELECT * FROM rule where idGejala=$ddata";
-	$diagnosaa = mysql_query($sql) or exit("Error query: <b>".$sql."</b>.");
-	
-	foreach(array(mysql_fetch_assoc($diagnosaa)) as $data2){
-		$rl = $data2['idPenyakit'];
-		echo $rl;
-
-		echo "   =    ";
-		$sql = "SELECT * FROM rule where idPenyakit=$rl AND idGejala=$ddata";
-		$rulee = mysql_query($sql) or exit("Error query: <b>".$sql."</b>.");
-		
-		// $sql1 = "INSERT INTO jumlah_prob (idPenyakit,Jumlah) SELECT idPenyakit,Probabilitas_gejala FROM rule WHERE idPenyakit=$rl AND idGejala=$ddata ";
-		// mysql_query($sql1) or exit("Error query : <b>".$sql1."</b>.");
-		
-		foreach (array(mysql_fetch_assoc($rulee)) as $data3)
-		{
-			$probb = $data3['Probabilitas_gejala'];
-			echo $probb;
-			echo "<br>";
-		}
-		 
-		// $sql1 = "Select probabilitas_penyakit from penyakit where idPenyakit=$rl";
-		// $py = mysql_query($sql1) or exit("Error query : <b>".$sql1."</b>");
-		// $jml = 1;
-		// foreach ($a = 1; $a < $pr; $a++ )
-		// {
-			// $jml = $jml * $pr;
-			// $jmlh = $py * $jml;
-			// $jml;
-			// echo $jmlh;
-		// }
-    ?>
 
     <?php
-		 }
+        include "config/koneksi.php";
 
-	?>
-	
-	<?php
-		 }
-		 $sql = "SELECT count(idPenyakit) as juml FROM jumlah_prob GROUP by idPenyakit";
-		 $qu = mysql_query($sql);
-		 $ct = mysql_fetch_row($qu);
-		
-		 foreach (mysql_fetch_assoc($qu) as $kb)
-		 {
-		 $sql1 = "SELECT COUNT(Jumlah) as hitung from jumlah_prob WHERE idPenyakit=$rl";
-		 $z = mysql_query($sql1)or exit("Salah Goblok");
-		 $x = mysql_fetch_assoc($z);
-		 echo $x['hitung'] ;
-		 
-		 // for ($i1 =0 ;$i1 < $ct ; $i1++)
-		 // {
-			 // for($j = 0 ; $j <= $x ; $j++){
-				 // $sql = "Select Jumlah from jumlah_prob where idPenyakit=$rl";
-				 // $hitung = mysql_query($sql) or exit("Error Goblok");
-				 // $hasil = 1;
-				 // $hasil = $hasil*$hitung;
-				 
-			 // }
-			 // $i1++;
-			  // echo $hasil;
-		 // }
-		 // $i++;
-		 }
-	?>
-	
-<!--	rumusan akhir	select s.GejalaPenyakit,p.Penyakit,p.Penanganan, s.Penjelasan  from gejalapenyakit s INNER JOIN penyakit p on s.idPenyakit=p.idPenyakit where s.idPenyakit = 1 -->
-	
-<!--
-      
-   
+        //reset data
+        $delprob   = "DELETE FROM jumlah_prob";
+        mysql_query($delprob) or exit("Error query: <b>".$sql."</b>.");
+        $dekprobakhir   = "DELETE FROM jumlah_prob_akhir";
+        mysql_query($dekprobakhir) or exit("Error query: <b>".$sql."</b>.");
+        $resetprob = "ALTER TABLE jumlah_prob AUTO_INCREMENT =1";
+        mysql_query($resetprob) or exit("Error query: <b>".$sql."</b>.");
+        $resetprobakhir = "ALTER TABLE jumlah_prob_akhir AUTO_INCREMENT =1";
+        mysql_query($resetprobakhir) or exit("Error query: <b>".$sql."</b>.");
 
-    function hitung_dinamis_1(){
-      $this->m_crud->truncate_data('jumlah_prob_final');
-      $this->m_crud->truncate_data('jumlah_prob');
-      
-      //daftar gejala yang dipilih
-      echo ' gejala yang dipilih : '.$gejala = implode(",",$this->input->post('gejala'));
-      echo "<br><br>";
-      $all_diagnosa = $this->model_diagnosa->all_diagnosa_dinamis($gejala);
-      
-      foreach ($all_diagnosa as $data) {
-        $rule = $this->model_diagnosa->rule_dinamis($data['idpenyakit'],$gejala);
-        $jumlah_probabilitas= 1;
+        //get data gejala from user
+        $dlpenyakit = $_POST['Gejala'];
+        //echo json_encode($dlpenyakit);
+    ?>
         
-        foreach ($rule as $data1) {
-            $probabilitas_gejala = $data1['probabilitas_gejala'];
-          
-            echo $data1['probabilitas_gejala'];
+    <!--get and show idGejala and idPenyakit-->
+    <?php
+        foreach($dlpenyakit as $ddata){
+            $sqlgetidGejala   = "SELECT * FROM rule where idGejala=$ddata ";
+            $getidGejala = mysql_query($sqlgetidGejala) or exit("Error query: <b>".$sql."</b>.");
+            while($row = mysql_fetch_assoc($getidGejala)){
+                $idgejala1= $row['idGejala'];
+                //echo ".";   
+                $idpenyakit1 = $row['idPenyakit'];
+                //echo "<br>";
+                //make array idPenyakit for distinct
+                $idpenyakit2[] = $row['idPenyakit'];
 
-            // perkalian prob gejala
-            $jumlah_probabilitas = $jumlah_probabilitas*$probabilitas_gejala;
-            echo ' = '.$jumlah_probabilitas;
-            echo "<br>";
-          
+                //Query for get data from rule
+                $sqlgetdatajmlProb = "SELECT * From rule 
+                                        WHERE idGejala = $idgejala1 and idPenyakit=$idpenyakit1" ;
+                $getdatajmlProb = mysql_query($sqlgetdatajmlProb) or exit("Error query: <b>".$sql."</b>.");           
+                while ($row = mysql_fetch_assoc($getdatajmlProb)){
+                    $idpenyakitProb = $row['idPenyakit'];
+                    $jumlahProb = $row['Probabilitas_gejala'];
+                    //Query insert data idPenyakit and jumlahProb from rule to jumlah_prob
+                    $sqlinsertdatajmlProb = "INSERT INTO jumlah_prob (idPenyakit, Jumlah) values ('$idpenyakitProb','$jumlahProb')";
+                    mysql_query($sqlinsertdatajmlProb) or exit("Error query : <b>".$sql."</b>."); 
+                } 
+            }      
         }
-
-        $idpenyakit = $this->model_diagnosa->idpenyakit($data['idpenyakit']);
-        
-        //perkalian prob gejala dengan dan di setiap penyakit
-        echo $idpenyakit['probabilitas_penyakit']."x".$jumlah_probabilitas;
-        
-        // hasil perkalian prob gejala dengan dan di setiap penyakit
-        $jum = $idpenyakit['probabilitas_penyakit'] * $jumlah_probabilitas;
-        echo " = ".$jum;
-        echo "<br><br>";
-        echo "<hr>";
-
-        $data = array(
-            'idpenyakit' => $data1['idpenyakit'],
-            'jumlah' => $jum,
-        );
-        $this->m_crud->insert_data('jumlah_prob', $data);
-
-        //penjumlahan prob semua penyakit
-        $jumlah_perkalian_jumlah_probabilitas = $this->model_diagnosa->jum_prob();
-        
-   
-      }
-        echo $jumlah_perkalian_jumlah_probabilitas['jumlah'].'<hr>';
-        
-        $jumlah_prob = $this->model_diagnosa->jumlah_prob();
-        
-        foreach ($jumlah_prob as $data2) {
-          $jumlah = $data2['jumlah'];
-          
-          //pembagian setiap penyakit / pencarian hasil akhir di setiap penyakit
-          echo "<br>".$jumlah."/".$jumlah_perkalian_jumlah_probabilitas['jumlah'];
-          
-          //hasil pembagian setiap penyakit / pencarian hasil akhir di setiap penyakit
-          $jumlah_final = $jumlah / $jumlah_perkalian_jumlah_probabilitas['jumlah']; 
-          echo " = ".$jumlah_final;
-          $data = array(
-              'idpenyakit' => $data2['idpenyakit'],
-              'jumlah' => $jumlah_final,
-          );
-          $this->m_crud->insert_data('jumlah_prob_final', $data);
-
+    ?>
+    
+    <?php
+    //Make new array from idPenyakit = Distinct
+        echo"<br>";
+        $imidPenyakit = implode(",",$idpenyakit2);
+        $exidPenyakit = explode(",",$imidPenyakit);
+        $dataPenyakitsort = array_unique($exidPenyakit);
+        $sort = sort($dataPenyakitsort);
+        for($x = 0; $x < count($dataPenyakitsort); $x++) {
+            $newdata = $dataPenyakitsort[$x];
+            $newidPenyakit[] = $dataPenyakitsort[$x];
+            //echo "<br>";
+            $sqlidprobPenyakit = "SELECT * FROM penyakit where idPenyakit=$newdata";
+            $idprobPenyakit = mysql_query($sqlidprobPenyakit) or exit("Error query : <b>".$sql."</b>."); 
+            while($row = mysql_fetch_assoc($idprobPenyakit)){
+            $getprobPenyakit[] = $row['Probabilitas_penyakit'];
+            }
         }
+    ?>
+    <?php
+        //get multiply data from propbabilitas_penyakit * probabilitas_gejala
+        $sqlhtgdatajmlProb = "SELECT idPenyakit, EXP(SUM(log(Jumlah))) as Jumlah_Probabilitas
+            FROM jumlah_prob GROUP by idPenyakit";
+        $htgdatajmlProb = mysql_query($sqlhtgdatajmlProb) or exit("Error query : <b>".$sql."</b>.");          
+        while($row = mysql_fetch_assoc($htgdatajmlProb)){
+            $jmlProb[] =  $row['Jumlah_Probabilitas'] .";";
+        }
+    -   
+        //make data array
+        $improbPenyakit = implode(",",$getprobPenyakit);
+        $exprobPenyakit = explode(",",$improbPenyakit);
+        $improbtot = implode(",",$jmlProb);
+        $exprobtot = explode(",",$improbtot);
+        //hitung total Prop masing-masing penyakit
+        for($i=0; $i < count($exprobPenyakit); $i++){
+            $hasil[$i] = $exprobPenyakit[$i] * $exprobtot[$i];     
+        }
+        //echo "<br>";
+        $imhasi = implode(",",$newidPenyakit);
+        $exhasi = explode(",",$imhasi);
+        $imhasil = implode(",",$hasil);
+        $exhasil = explode(",",$imhasil);
+        $hehe = array_combine($exhasi,$exhasil);
+        //print_r($hehe);
+        foreach($hehe as $key=>$value){
+            $sqlinsertidPenyakit = "INSERT INTO jumlah_prob_akhir (idPenyakit,Jumlah) 
+                                        VALUES ('$key','$value') ";
+            mysql_query($sqlinsertidPenyakit) or exit("Error query : <b>".$sql."</b>."); 
+        }
+    ?>
+<table class="table table-bordered">
+<tr>
+<th>Jenis Penyakit</th>
+<th>Penanganan</th>
+</tr>
+    <?php 
+        $sqlpenyakit = "SELECT a.idPenyakit, a.Penyakit, a.Penanganan, Min(b.Jumlah) 
+        FROM penyakit as a
+        JOIN jumlah_prob_akhir AS b 
+        ON a.idPenyakit = b.idPenyakit";
+        $penyakit = mysql_query($sqlpenyakit) or exit("Error query : <b>".$sql."</b>."); 
+        while($row = mysql_fetch_assoc($penyakit)){
+       ?><tr> 
+			<td><?php echo $row['Penyakit'];?></td> 
+			<td><?php echo $row['Penanganan'];?></td>
+        </tr>
+		<?php 
+		}
+     ?>
+     </table>      
+<!--   
+        echo "Jenis Penyakit:" .$row['Penyakit'] ."<br>" ."Penanganan :" ."<br>" .$row['Penanganan'];
+     }  
+?>-->
+    </br>
+    <button>
+        <a href = "Diagnosa.php">
+            Exit
+        </a>
+    </button>
 
-        $max_prob = $this->model_diagnosa->max_prob();
-        echo "<hr>";
-
-        // memilih nilai terbesar dari perhitungan
-        echo $max_prob['jumlah'];
-        echo json_encode($max_prob);
-        
-        // hasil diagnosa penyakit
-        $hasil = $this->model_diagnosa->hasil($max_prob['idpenyakit']);
-        echo "<br>".$hasil['penyakit'];
-        echo json_encode($hasil);
-    } 
-}
+    <footer class="footer clearfix">
+		<div class="article">
+	    	<br />
+			<div align="center">Copyright&copy; Pakar Kakao 2019. All Right Reserved.</div>
+		</div>
+    </footer>
+</body>
+</html>
